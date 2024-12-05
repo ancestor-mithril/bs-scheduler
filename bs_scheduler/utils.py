d48461ab9512903d50704961526c2505d9e8f559
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2024-12-05 21:45:46 +0200

    Moved utils into separate file

--- bs_scheduler/utils.py
+++ bs_scheduler/utils.py
@@ -1,3 +1,19 @@
 def check_isinstance(x, instance: type):
     if not isinstance(x, instance):
         raise TypeError(f"{type(x).__name__} is not a {instance.__name__}.")
+
+
+def rint(x: float) -> int:
+    """ Rounds to the nearest int and returns the value as int.
+    """
+    return int(round(x))
+
+
+def clip(x: int, min_x: int, max_x: int) -> int:
+    """ Clips x to [min, max] interval.
+    """
+    if x < min_x:
+        return min_x
+    if x > max_x:
+        return max_x
+    return x
